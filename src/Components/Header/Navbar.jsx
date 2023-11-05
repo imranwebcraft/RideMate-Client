4f0ec08aac0a1a03f028a0c2668d82359cf80da0
Imran Hossain Sakil <112363382+imran-it1@users.noreply.github.com>
2023-11-05 17:11:34 +0600

    Dark mode toggole implement

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,7 +1,32 @@
 import { Link, NavLink } from 'react-router-dom';
 import logoImg from '../../assets/Images/rideMate.png';
 import Container from '../Container/Container';
+import { useEffect, useState } from 'react';
 const Navbar = () => {
+	// Dark mode control
+	const [theme, setTheme] = useState('light');
+	const html = document.documentElement;
+
+	const toggoleTheme = () => {
+		if (theme === 'light') {
+			html.classList.remove('light');
+			html.classList.add('dark');
+			setTheme('dark');
+			localStorage.setItem('theme', 'dark');
+		} else {
+			html.classList.remove('dark');
+			html.classList.add('light');
+			setTheme('light');
+			localStorage.setItem('theme', 'light');
+		}
+	};
+
+	useEffect(() => {
+		const currentTheme = localStorage.getItem('theme');
+		html.classList.add(currentTheme);
+		setTheme(currentTheme);
+	}, [html.classList]);
+
 	return (
 		<header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white border-b border-gray-200 text-sm py-3 sm:py-0 dark:bg-gray-800 dark:border-gray-700">
 			<Container>
@@ -148,7 +173,7 @@ const Navbar = () => {
 							<Link to={'/login'}>
 								<button
 									type="button"
-									className="py-2 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-gray-500 font-medium text-gray-500 dark:text-gray-400 hover:text-white hover:bg-sky-500 hover:border-sky-500 focus:border-sky-500 focus:outline-none focus:ring-2 focus:ring-sky-200 focus:ring-offset-2 transition-all duration-300 text-sm dark:focus:ring-offset-gray-800 m-2 md:mx-2"
+									className="py-2 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-gray-500 font-medium text-gray-500 dark:text-gray-400 hover:text-white hover:bg-sky-500 hover:border-sky-500 focus:border-sky-500 focus:outline-none focus:ring-2 focus:ring-sky-200 focus:ring-offset-2 transition-all duration-300 text-sm dark:focus:ring-offset-gray-800"
 								>
 									<svg
 										className="w-4 h-4"
@@ -163,6 +188,37 @@ const Navbar = () => {
 									Login
 								</button>
 							</Link>
+
+							{/* Dark mode toggole */}
+
+							<button
+								onClick={toggoleTheme}
+								className="group inline-flex flex-shrink-0 justify-center items-center h-9 w-9 font-medium rounded-full text-gray-800 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700 hover:cursor-pointer"
+							>
+								{theme === 'light' ? (
+									<svg
+										className="w-4 h-4"
+										xmlns="http://www.w3.org/2000/svg"
+										width="16"
+										height="16"
+										fill="currentColor"
+										viewBox="0 0 16 16"
+									>
+										<path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z"></path>
+									</svg>
+								) : (
+									<svg
+										className="w-4 h-4"
+										xmlns="http://www.w3.org/2000/svg"
+										width="16"
+										height="16"
+										fill="currentColor"
+										viewBox="0 0 16 16"
+									>
+										<path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"></path>
+									</svg>
+								)}
+							</button>
 						</div>
 					</div>
 				</nav>
